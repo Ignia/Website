6762e5c991d3266aa6bae6783bb552618dfd3712
Katherine Trunkey <ktrunkey@gmail.com>
2018-02-14 15:49:44 -0800

    Moved homepage navigation arrows smooth scrolling to JavaScript

--- Ignia.Web/Shared/Scripts/Home.js
+++ Ignia.Web/Shared/Scripts/Home.js
@@ -5,4 +5,23 @@
  * @author Katherine Trunkey (katherine.trunkey@ignia.com)
  */
 
-"use strict";
+$(function() {
+  'use strict';
+
+  /**
+   * Establish variables
+   */
+  var topOffset                 = $('#Header').height();
+
+  /**
+   * Initializes Foundation smooth scrolling for homepage navigation arrows
+   */
+  var
+    smoothScrollOptions         = {
+      animationDuration         : 750,
+      offset                    : (topOffset - 25)
+    },
+    splashSmoothScroll          = new Foundation.SmoothScroll($('#SplashArrow'), smoothScrollOptions),
+    clientsSmoothScroll         = new Foundation.SmoothScroll($('#ClientsArrow'), smoothScrollOptions);
+
+});
