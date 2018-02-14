cd6071f4eb59daed6b54f080640e4cc1d54c723e
Katherine Trunkey <ktrunkey@gmail.com>
2018-02-14 09:15:58 -0800

    Added navigation menu highlight / active handling

--- Ignia.Web/Shared/Scripts/Menu.js
+++ Ignia.Web/Shared/Scripts/Menu.js
@@ -1,9 +1,76 @@
 ﻿/**
- * HEADER NAVIGATION
+ * MENU / NAVIGATION
  *
- * @file Functions associated with the primary navigation, including highlight, smooth scrolling, and menu toggle functionality.
+ * @file Functions associated with the primary navigation, including highlight and menu toggle functionality.
  * @author Katherine Trunkey (katherine.trunkey@ignia.com)
  */
 
-"use strict";
+/**
+ * Removes the "active" class from all primary navigation items
+ */
+function resetNavHighlight() {
+  'use strict';
+  $('#PrimaryNavigation ul li a').removeClass('active');
+};
 
+$(function () {
+  'use strict';
+
+  /**
+   * Establish variables
+   */
+  var
+    topOffset                   = $('#Header').height(),
+    headingBuffer               = $('#Introduction header:first-child').height();
+
+  /**
+   * Fires navigation highlight reset on initialization
+   */
+  resetNavHighlight();
+
+  /**
+   * Highlights the "active" navigation as the corresponding panel comes into view
+   */
+  $(window).scroll(function() {
+
+    var scrollPosition          = $(window).scrollTop() + topOffset + headingBuffer;
+
+    // Highlight 'About (01)' item
+    if (scrollPosition >= $("#Introduction").offset().top && scrollPosition <= $("#Services").offset().top) {
+      resetNavHighlight();
+      $('#IntroductionAnchor, #IntroductionAnchorSmallScreen').addClass('active');
+    }
+
+    // Highlight 'Services (02)' item
+    if (scrollPosition >= $("#Services").offset().top && scrollPosition <= $("#ClientHighlights").offset().top) {
+      resetNavHighlight();
+      $('#ServicesAnchor, #ServicesAnchorSmallScreen').addClass('active');
+    }
+
+    // Highlight 'Clients (03)' item
+    if (scrollPosition >= $("#ClientHighlights").offset().top && scrollPosition <= $("#Contact").offset().top) {
+      resetNavHighlight();
+      $('#ClientHighlightsAnchor, #ClientHighlightsAnchorSmallScreen').addClass('active');
+    }
+
+    // Highlight 'Contact (04)' item
+    if (scrollPosition >= $('#Contact').offset().top) {
+      resetNavHighlight();
+      $('#ContactAnchor, #ContactAnchorSmallScreen').addClass('active');
+    }
+
+  });
+
+  /**
+   * Sets the active state of the tapped-on navigation item;
+   * closes the off-canvas menu on tap of one of the navigation items
+   */
+  $('#PrimaryNavigationSmallScreen ul li a').click(function (event) {
+    resetNavHighlight();
+    $(this).addClass('active');
+    setTimeout(function () {
+      $('#PrimaryNavigationSmallScreen').foundation('close');
+    }, 250);
+  });
+
+});
