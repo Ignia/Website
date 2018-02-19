daddc284ffca1d190d2c29ec4478ccc89cfb5a61
Katherine Trunkey <ktrunkey@gmail.com>
2018-02-19 10:24:51 -0800

    Added homepage animation scripts

--- Ignia.Web/Shared/Scripts/Home.Animations.js
+++ Ignia.Web/Shared/Scripts/Home.Animations.js
@@ -1,1 +1,20 @@
-﻿
+﻿/**
+ * HOME: ANIMATIONS
+ *
+ * @file Client-side functionality defining animations for the home page, such as the panel wipes, "factory" movement, and
+ * client logos.
+ * @author Katherine Trunkey (katherine.trunkey@ignia.com)
+ */
+
+$(function() {
+  'use strict';
+
+  /**
+   * Establish variables
+   */
+  var
+    topOffset                   = $('#Header').height(),
+    slides                      = $('.panel').not('.splash'),
+    sceneController             = new ScrollMagic.Controller();
+
+});
