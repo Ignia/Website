d82c933557bde56deb894c3ba05f63c1c71a9c56
Katherine Trunkey <ktrunkey@gmail.com>
2018-02-19 10:25:53 -0800

    Added tweens for client logos

--- Ignia.Web/Shared/Scripts/Home.Animations.js
+++ Ignia.Web/Shared/Scripts/Home.Animations.js
@@ -17,4 +17,23 @@ $(function() {
     slides                      = $('.panel').not('.splash'),
     sceneController             = new ScrollMagic.Controller();
 
+  /**
+   * Creates scale and opacity tweens for featured client logos
+   */
+  var
+    clientLogosItem             = $('#ClientHighlights section.logos ul li'),
+    clientLogosDuration         = ($('#ClientHighlights').innerHeight()*0.8),
+    clientLogosTween            = TweenMax.staggerFromTo(
+      clientLogosItem,
+      3,
+      { autoAlpha: 0, scale: 0 },
+      { autoAlpha: 1, scale: 1, ease: Power2.easeIn, autoCSS: true },
+      0.25
+    ),
+    clientLogosScene            = new ScrollMagic.Scene({
+      triggerElement            : '#ClientHighlights section.logos',
+      duration                  : clientLogosDuration,
+      triggerHook               : 1,
+    }).setTween(clientLogosTween).addTo(sceneController);
+
 });
