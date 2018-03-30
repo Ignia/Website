c52382010bfa515443caef2b2ef83b742d115840
Katherine Trunkey <ktrunkey@gmail.com>
2018-03-30 15:22:43 -0700

    Established `HomeTopicViewModel`

---
+++ Ignia.Web/Models/HomeTopicViewModel.cs
@@ -0,0 +1,20 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Website
+\=============================================================================================================================*/
+using Ignia.Topics.ViewModels;
+
+namespace Ignia.Web.Models {
+
+  /*============================================================================================================================
+  | VIEW MODEL: HOME TOPIC
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Provides a strongly-typed data transfer object for feeding views with information about a <c>Home</c> topic.
+  /// </summary>
+  public class HomeTopicViewModel: PageTopicViewModel {
+
+  } // Class
+
+} // Namespace
