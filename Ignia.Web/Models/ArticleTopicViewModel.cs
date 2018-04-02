8d6a7c39291a9d86a5306ad69543b8785b1ad3d3
Katherine Trunkey <ktrunkey@gmail.com>
2018-04-02 10:46:45 -0700

    Established `ArticleTopicViewModel`

---
+++ Ignia.Web/Models/ArticleTopicViewModel.cs
@@ -0,0 +1,30 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Website
+\=============================================================================================================================*/
+using Ignia.Topics.ViewModels;
+
+namespace Ignia.Web.Models {
+
+  /*============================================================================================================================
+  | VIEW MODEL: ARTICLE TOPIC
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Provides a strongly-typed data transfer object for feeding views with information about a <c>Article</c> topic.
+  /// </summary>
+  /// <remarks>
+  ///   It is not expected that any topics will directly implement the <c>Article</c> content type that corresponds
+  ///   to this view model. Instead, it provides a base schema definition for, e.g.,
+  ///   <see cref="TechnologyAdvisoryTopicViewModel"/>.
+  /// </remarks>
+  public class ArticleTopicViewModel : PageTopicViewModel {
+
+    public bool IsFeatured { get; set; }
+    public string Author { get; set; }
+    public string PublishDate { get; set; }
+    public string Abstract { get; set; }
+
+  } // Class
+
+} // Namespace
