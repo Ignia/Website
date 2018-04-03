58c76a43d376f05ac7b17af7479b1f36f9464369
Katherine Trunkey <ktrunkey@gmail.com>
2018-04-03 11:08:05 -0700

    Established view models

---
+++ Ignia.Web/Models/StandardTopicViewModel.cs
@@ -0,0 +1,19 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Website
+\=============================================================================================================================*/
+
+namespace Ignia.Web.Models {
+
+  /*============================================================================================================================
+  | VIEW MODEL: STANDARD TOPIC
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Provides a strongly-typed data transfer object for feeding views with information about a <c>Standard</c> topic.
+  /// </summary>
+  public class StandardTopicViewModel : ArticleTopicViewModel {
+
+  } // Class
+
+} // Namespace
