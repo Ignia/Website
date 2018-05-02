cb79dbabb12762be3b39239bebb1289a33b180a2
Katherine Trunkey <ktrunkey@gmail.com>
2018-05-02 12:06:19 -0700

    Introduced pin scene to Article metadata

---
+++ Ignia.Web/Shared/Scripts/Library.Animations.js
@@ -0,0 +1,34 @@
+﻿/**
+ * LIBRARY: ANIMATIONS
+ *
+ * @file Client-side functionality defining animations for Library pages, such as the pinning of the Article metadata.
+ * @author Katherine Trunkey (katherine.trunkey@ignia.com)
+ */
+
+$(function () {
+  'use strict';
+
+  /**
+   * Establish variables
+   */
+  var
+    topOffset                   = $('#Header').outerHeight(),
+    mainContentHeight           = ($(window).height() - topOffset),
+    sceneController             = new ScrollMagic.Controller();
+
+  /**
+   * Set scenes for tablet and larger screens
+   */
+  if ($(window).width() > 767) {
+
+    // Article metadata pin
+    var articleMetadataPinScene = new ScrollMagic.Scene({
+      triggerElement            : '#References',
+      triggerHook               : 0,
+      offset                    : (-topOffset - 84),
+      duration                  : ($('#ArticleBody').height() - $('#References').height() -64)
+    }).setPin('#References', { pushFollowers: false }).addTo(sceneController);
+
+  }
+
+});
