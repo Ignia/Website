163d81110d97fddb9b4e80c91e563d542b54019f
JeremyCaney <jeremy.caney@ignia.com>
2020-10-06 11:51:45 -0700

    Preferred implicit variable typing

--- Controllers/ContactController.cs
+++ Controllers/ContactController.cs
@@ -47,17 +47,18 @@ namespace Ignia.Web.Controllers {
         /*------------------------------------------------------------------------------------------------------------------------
         | Send out email
         \-----------------------------------------------------------------------------------------------------------------------*/
-        StringBuilder mailBody = new StringBuilder();
+        var mailBody = new StringBuilder();
         mailBody.Append("<div style=\"text-align: center; padding-top: 0px; padding-right: 0px; padding-bottom: 50px; padding-left: 0px;\"><img src=\"http://test.ignia.com/Images/Logo.png\" alt=\"Ignia\" /></div>");
         mailBody.Append("<h1 style=\"font-family: Arial,sans-serif; font-weight: normal;\">Information request from <a href=\"mailto:" + fromEmail + "?Subject=Thank%20you%20for%20inquiring%20about%20Ignia\" style=\"color: #EF4A24;\">" + fromName + "</a></h1>");
         mailBody.Append("<h2 style=\"font-family: Arial,sans-serif; font-weight: normal;\">Message:</h2>");
         mailBody.Append("<div style=\"font-family: Arial,sans-serif; font-size: 16px; line-height: 18px; padding-top: 15px;\">" + messageBody + "</div>");
 
-        SmtpClient smtp         = new SmtpClient();
-        MailMessage mailMessage = new MailMessage(new MailAddress(fromEmail), new MailAddress("ktrunkey@gmail.com"));
-        mailMessage.Subject     = "Contact from Ignia.com";
-        mailMessage.IsBodyHtml  = true;
-        mailMessage.Body        = "<html><head><title>Email Message from Ignia.com</title></head><body style=\"background-color: #FFF; padding-top: 50px; padding-right: 35px; padding-bottom: 50px; padding-left: 35px; margin-top: 0px; margin-right: 0px; margin-bottom: 0px; margin-left: 0px; border: 1px solid #F0AD20;\"> " + mailBody + "</body></html>";
+        var smtp                = new SmtpClient();
+        var mailMessage         = new MailMessage(new MailAddress(fromEmail), new MailAddress("info@Ignia.com")) {
+          Subject               = "Contact from Ignia.com",
+          IsBodyHtml            = true,
+          Body                  = "<html><head><title>Email Message from Ignia.com</title></head><body style=\"background-color: #FFF; padding-top: 50px; padding-right: 35px; padding-bottom: 50px; padding-left: 35px; margin-top: 0px; margin-right: 0px; margin-bottom: 0px; margin-left: 0px; border: 1px solid #F0AD20;\"> " + mailBody + "</body></html>"
+        };
 
         smtp.Send(mailMessage);
 
