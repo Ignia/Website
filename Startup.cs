8d1dc4aa85be99d3e3fc2593039cb76e34f2096c
JeremyCaney <jeremy.caney@ignia.com>
2020-10-06 11:40:19 -0700

    Established OpenID Connect for Azure AD
    The legacy forms based authentication (FBA) is no longer supported in ASP.NET Core. Instead, we'll be replacing it with the basic OpenID Connect for Azure Active Directory, which integrates directly with Ignia's existing Azure authentication, similar to our Office 365 profiles.

--- Startup.cs
+++ Startup.cs
@@ -69,6 +69,32 @@ namespace Ignia.Web {
     public void ConfigureServices(IServiceCollection services) {
 
       /*------------------------------------------------------------------------------------------------------------------------
+      | Use the Microsoft Identity Platform for authentication
+      >-------------------------------------------------------------------------------------------------------------------------
+      | ### NOTE JJC20191122: OpenId only allows authentication against a single Azure AD tenant, or ALL Azure AD tenants. In
+      | order to permit authentication against just Ignia, we to validate the issuer. The issuer addresses can be retrieved per
+      | tenant by looking at the "issuer" attribute of the https://login.microsoftonline.com/{TenantId}/v2.0/.well-known/openid-
+      | configuration feed (where {TenantId} is e.g. "Ignia.com").
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      services.AddAuthentication(options => {
+        options.DefaultAuthenticateScheme = OpenIdConnectDefaults.AuthenticationScheme;
+        options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+      })
+      .AddOpenIdConnect(options => {
+        Configuration.GetSection("OpenIdConnect").Bind(options);
+        options.CorrelationCookie.SameSite = SameSiteMode.None;
+        options.SaveTokens = true;
+        options.TokenValidationParameters = new TokenValidationParameters {
+          NameClaimType = "name",
+          ValidIssuers = new[] {
+            $"https://login.microsoftonline.com/10dcd9d4-80f7-47c8-ad5a-7efddcd5f868/v2.0",
+          }
+        };
+      })
+      .AddCookie();
+
+      /*------------------------------------------------------------------------------------------------------------------------
       | Configure: MVC
       \-----------------------------------------------------------------------------------------------------------------------*/
       var mvcBuilder = services.AddControllersWithViews()
